9d11d535eea2c1516ac24cf5b46a2984321e39b3
Adeen Mir <adeenmir991@gmail.com>
2026-02-09 10:27:34 +0500

    First Lab

---
+++ file1.py
@@ -0,0 +1,6 @@
+# Code to take 2 numbers and show their sum
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}")
